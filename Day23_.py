67a64c2410d8963e0b50b15e1cf7e1f3b25da538
Bishnupriyap01 <86412967+Bishnupriyap01@users.noreply.github.com>
2023-08-30 15:00:33 +0530

    Create Day23_.py

---
+++ Day23_.py
@@ -0,0 +1,13 @@
+#https://leetcode.com/problems/customer-placing-the-largest-number-of-orders/?envType=study-plan-v2&envId=30-days-of-pandas&lang=pythondata
+import pandas as pd
+
+def largest_orders(orders: pd.DataFrame) -> pd.DataFrame:
+    # Group by customer_number and count the number of orders for each customer
+    customer_order_counts = orders.groupby('customer_number')['order_number'].count().reset_index()
+    
+    # Find the customer with the largest number of orders
+    max_orders_customer = customer_order_counts[customer_order_counts['order_number'] == customer_order_counts['order_number'].max()][['customer_number']]
+    
+    return max_orders_customer
+
+     
