7a51d0a7d4fad09dc1b5afafa32816fb0143b586
Bishnupriyap01 <86412967+Bishnupriyap01@users.noreply.github.com>
2023-08-30 15:02:54 +0530

    Create Day25_.py

---
+++ Day25_.py
@@ -0,0 +1,11 @@
+#https://leetcode.com/problems/daily-leads-and-partners/?envType=study-plan-v2&envId=30-days-of-pandas&lang=pythondata
+import pandas as pd
+
+def daily_leads_and_partners(daily_sales: pd.DataFrame) -> pd.DataFrame:
+    # Group the daily sales by date_id and make_name, and count distinct lead_id's and partner_id's
+    grouped = daily_sales.groupby(['date_id', 'make_name']).agg({'lead_id': 'nunique', 'partner_id': 'nunique'}).reset_index()
+    
+    # Rename the columns for clarity
+    grouped.columns = ['date_id', 'make_name', 'unique_leads', 'unique_partners']
+    
+    return grouped
