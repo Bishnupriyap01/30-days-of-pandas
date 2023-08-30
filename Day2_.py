64721acb41d727af71bba6c4525cf2171fe10bbd
Bishnupriyap01 <86412967+Bishnupriyap01@users.noreply.github.com>
2023-08-30 14:05:49 +0530

    Update Day2_.py

--- Day2_.py
+++ Day2_.py
@@ -1,1 +1,8 @@
+#problem link _ https://leetcode.com/problems/recyclable-and-low-fat-products/?envType=study-plan-v2&envId=30-days-of-pandas&lang=pythondata
 
+import pandas as pd
+
+def find_products(products: pd.DataFrame) -> pd.DataFrame:
+  product_df = products[(products['low_fats'] == 'Y') & (products['recyclable'] =='Y')]
+  result_df = product_df[['product_id']]
+  return result_df
