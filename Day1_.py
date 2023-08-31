650f12dd9eed2b0acbc2dfd43eab456e2e1aa0c2
Bishnupriyap01 <86412967+Bishnupriyap01@users.noreply.github.com>
2023-08-31 15:11:06 +0530

    Update Day1_.py

--- Day1_.py
+++ Day1_.py
@@ -1,5 +1,15 @@
 #Problem link _ https://leetcode.com/problems/big-countries/?envType=study-plan-v2&envId=30-days-of-pandas&lang=pythondata
 #595. Big Countries
+#A country is big if:
+
+# it has an area of at least three million (i.e., 3000000 km2), or
+# it has a population of at least twenty-five million (i.e., 25000000).
+# Write a solution to find the name, population, and area of the big countries.
+
+# Return the result table in any order.
+
+# The result format is in the following example.
+
 
 
 import pandas as pd
